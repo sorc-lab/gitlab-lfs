1abecc1cd5ab8a12a7b49525ae20b243c18a5f6f
macfisher <macfisher01@gmail.com>
2021-07-16 06:25:12 -0600

    Testing copyBkToDropbox

--- backup.py
+++ backup.py
@@ -19,18 +19,6 @@ def createBk():
     os.system("docker exec -t {} gitlab-backup create".format(CONTAINER_NAME));
 
 def getBkFileName():
-    # grabs everything before the 'T', e.g.: 2021-07-14T19:20:36.086270
-    # splitDateStr = datetime.now().isoformat().split("T", 1);
-    # isoDateStr = splitDateStr[0].replace("-", "_");
-
-    # TODO: This fails because BK_DIR is pointed to host machine path, not container
-    # find first backup file in dir that contains today's date
-    # for file in os.listdir(BK_DIR):
-    #     if fnmatch.fnmatch(file, "*{}*".format(isoDateStr)):
-    #         return file;
-
-    # TODO: Try: `docker exec --privileged $NEW_CONTAINER_ID ls -1 /var/log`
-    # return_value = os.popen('ls').read()
     lsDir = os.popen("docker exec {} ls -1 {}".format(CONTAINER_NAME, BK_DIR)).read();
     bkFiles = lsDir.splitlines();
 
@@ -39,50 +27,30 @@ def getBkFileName():
 
     for bkFile in bkFiles:
         if bkFile.endswith(".tar"):
-            print("Found a file w/ '.tar' ext.: " + bkFile);
-
             # get Unix timestamp portion of file name
             splitFileName = bkFile.split("_", 1);
-            timestamp = int(splitFileName[0]); # cast to long
-            print(timestamp); # TODO: Remove after testing
+            timestamp = int(splitFileName[0]);
 
             if timestamp > latestTimestamp:
                 latestTimestamp = timestamp;
                 fileName = bkFile;
 
-    if fileName is None:
-        print("No backup files found!");
-
-    print(fileName);
+    return fileName;
 
 
 def copyBkToDropbox():
-    bkFileName = getBkFileName();
+    fileName = getBkFileName();
 
-    if bkFileName is None:
+    if fileName is None:
         print("No backup file found for current iso date string.");
         return;
 
     # extract the bk file from docker container first
-    os.system("docker cp {}:{}/{} .".format(CONTAINER_NAME, BK_DIR, bkFileName));
+    os.system("docker cp {}:{}/{} .".format(CONTAINER_NAME, BK_DIR, fileName));
 
     # TODO: Test that this copies to Dropbox home first, then modify.
-    os.system("scp -rp {}@{}:~/".format(DROPBOX_USR_NAME, DROPBOX_IP_ADDR));
+    os.system("scp -rp {} {}@{}:~/".format(fileName, DROPBOX_USR_NAME, DROPBOX_IP_ADDR));
 
 
 #createBk();
-#copyBkToDropbox();
-
-# TODO: REMOVE AFTER TESTING. copyBkToDropbox will call this method internally.
-getBkFileName();
-
-# *** STACK TRACE FROM FIRST TEST RUN ***
-# Backup task is done.
-# Traceback (most recent call last):
-#   File "backup.py", line 45, in <module>
-#     copyBkToDropbox();
-#   File "backup.py", line 31, in copyBkToDropbox
-#     bkFileName = getBkFileName();
-#   File "backup.py", line 26, in getBkFileName
-#     for file in os.listdir(BK_DIR):
-# FileNotFoundError: [Errno 2] No such file or directory: '/var/opt/gitlab/backups'
+copyBkToDropbox();
